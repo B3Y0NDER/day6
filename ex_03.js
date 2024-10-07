0b57606b5305c3faa8be6e79e66d719bdcc55970
B3Y0NDER <dine.terzi@epitech.digital>
2024-10-07 13:32:09 +0200

    Update ex_03.js

--- ex_03.js
+++ ex_03.js
@@ -1,1 +1,14 @@
-
+function addCarToAgent(agent){
+if (typeof agent.name !=="string"){
+console.warn("there is some keys missing or uncorrectly typed");
+return null
+}
+agent.car = "Aston Martin"
+shareThatBeauty(agent)
+return agent
+}
+function shareThatBeauty(agent){
+console.log("Agent partagé sur Instagram :",agent)
+}
+let agent= { name : "James Bond"}
+addCarToAgent(agent)
