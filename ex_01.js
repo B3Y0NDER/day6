05d42f828dc8802bd7669c589ddb0157cf45722e
B3Y0NDER <dine.terzi@epitech.digital>
2024-10-07 11:16:43 +0200

    Update ex_01.js

--- ex_01.js
+++ ex_01.js
@@ -1,11 +1,17 @@
 function displayAgentInfo(agent) {
-const text = "My name is " + agent.lastName + ", " + agent.firstName + " " + agent.lastName + "! I'm the agent " + agent.code + " and I'm " + agent.age + " years old.";
-document.body.innerHTML += "<p>" + text + "</p>";    
+const text = `My name is ${agent.lastName}, ${agent.firstName} ${agent.lastName}! I'm the agent ${agent.code} and I'm ${agent.age} years old.`;
+let p = document.querySelector('#container p');
+p.innerHTML = text   
 }
+
 var agent = {
 firstName: "James",
 lastName: "Bond",
 code: "007",
 age: 57
 };
-displayAgentInfo(agent);
+
+document.addEventListener('DOMContentLoaded', function() {
+    displayAgentInfo (agent);
+});
+
