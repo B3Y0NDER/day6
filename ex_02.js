be9f548388adbeabf46f3547fb4a5892234e74b2
B3Y0NDER <dine.terzi@epitech.digital>
2024-10-07 11:59:30 +0200

    Update ex_02.js

--- ex_02.js
+++ ex_02.js
@@ -1,1 +1,7 @@
-
+function getAgentCar (jsonString){
+const obj = JSON.parse(jsonString)
+const agent = obj.agent
+return agent.car
+}
+const jsonString = '{"agent":{"name": "John" , "car":"Toyota" }}';
+console.log(getAgentCar(jsonString)); 
